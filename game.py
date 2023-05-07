a661f264ed909998ed039fce8b72f0adf119794d
Nguyen Van Nam <nam.nv205106@gmail.com>
2023-05-07 18:01:30 +0700

    update piece image and logo

--- game.py
+++ game.py
@@ -8,12 +8,12 @@ TITLE = "Cờ Toán Việt Nam"
 FPS = 60
 FONT_NAME = 'arial'
 icon = 'img/logo.jfif'
-COLUMN = 11
-ROW = 9
+COLUMN = 9
+ROW = 11
 SQUARE_SIZE = 75
 
-WHITE = (238,238,210)
-BLACK = (118,150,86)
+WHITE = (238, 238, 210)
+BLACK = (118, 150, 86)
 
 
 class Game:
@@ -28,6 +28,34 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
+        # self.broad = [["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9"],
+        #               ["--", "--", "--", "--", "r0", "--", "--", "--", "--"],
+        #               ["--", "--", "--", "--", "--", "--", "--", "--", "--"],
+        #               ["--", "--", "--", "--", "--", "--", "--", "--", "--"],
+        #               ["--", "--", "--", "--", "--", "--", "--", "--", "--"],
+        #               ["--", "--", "--", "--", "--", "--", "--", "--", "--"],
+        #               ["--", "--", "--", "--", "--", "--", "--", "--", "--"],
+        #               ["--", "--", "--", "--", "--", "--", "--", "--", "--"],
+        #               ["--", "--", "--", "--", "--", "--", "--", "--", "--"],
+        #               ["--", "--", "--", "--", "r0", "--", "--", "--", "--"],
+        #               ["r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9"]]
+        self.board = [["b9", "--", "--", "--", "--", "--", "--", "--", "--", "--", "r1"],
+                      ["b8", "--", "--", "--", "--", "--", "--", "--", "--", "--", "r2"],
+                      ["b7", "--", "--", "--", "--", "--", "--", "--", "--", "--", "r3"],
+                      ["b6", "--", "--", "--", "--", "--", "--", "--", "--", "--", "r4"],
+                      ["b5", "b0", "--", "--", "--", "--", "--", "--", "--", "r0", "r5"],
+                      ["b4", "--", "--", "--", "--", "--", "--", "--", "--", "--", "r6"],
+                      ["b3", "--", "--", "--", "--", "--", "--", "--", "--", "--", "r7"],
+                      ["b2", "--", "--", "--", "--", "--", "--", "--", "--", "--", "r8"],
+                      ["b1", "--", "--", "--", "--", "--", "--", "--", "--", "--", "r9"]]
+
+
+        self.piece = ["b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9",
+                      "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9"]
+        self.piece_img = {}
+        for i in range(len(self.piece)):
+            self.piece_img[self.piece[i]] = pg.image.load("img/" + self.piece[i] + ".png").convert_alpha()
+            self.piece_img[self.piece[i]] = pg.transform.scale(self.piece_img[self.piece[i]], (75, 75))
 
     def new(self):
         # start a new game
@@ -41,7 +69,7 @@ class Game:
             self.clock.tick(FPS)
             self.events()
             self.update()
-            self.draw_broad()
+            self.draw_state()
 
     def update(self):
         # Game Loop - Update
@@ -56,13 +84,25 @@ class Game:
                     self.playing = False
                 self.running = False
 
-    def draw_broad(self):
-        for i in range(ROW):
-            for j in range(COLUMN):
+    def draw_board(self):
+        for i in range(COLUMN):
+            for j in range(ROW):
                 if (i + j) % 2 == 0:
                     pg.draw.rect(self.screen, WHITE, (i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
                 else:
                     pg.draw.rect(self.screen, BLACK, (i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    def draw_piece(self):
+        for i in range(COLUMN):
+            for j in range(ROW):
+                piece = self.board[i][j]
+                if piece != "--":
+                    self.screen.blit(self.piece_img[piece],
+                                     pg.Rect(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    def draw_state(self):
+        self.draw_board()
+        self.draw_piece()
         pg.display.flip()
 
     def show_start_screen(self):
@@ -74,5 +114,3 @@ class Game:
     def show_go_screen(self):
         # game over/continue
         pass
-
-
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame as pg
 import sys
 
 if __name__ == '__main__':
+
     g = game.Game()
     # g.show_start_screen()
     while g.running:
