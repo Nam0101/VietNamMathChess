37900696f19d65736a425e7648ac0c25ea223354
Nguyen Van Nam <nam.nv205106@gmail.com>
2023-05-07 10:51:52 +0700

    game and chess

---
+++ game.py
@@ -0,0 +1,74 @@
+import pygame as pg
+import sys
+from os import path
+
+WIDTH = 675
+HEIGHT = 825
+TITLE = "My Game"
+FPS = 60
+FONT_NAME = 'arial'
+COLUMN = 11
+ROW = 9
+SQUARE_SIZE = 75
+# Define Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 238, 238)
+
+
+class Game:
+    def __init__(self):
+        # initialize game window, etc
+        self.playing = None
+        self.all_sprites = None
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        # start a new game
+        self.all_sprites = pg.sprite.Group()
+        self.run()
+
+    def run(self):
+        # Game Loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw_broad()
+
+    def update(self):
+        # Game Loop - Update
+        self.all_sprites.update()
+
+    def events(self):
+        # Game Loop - events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+    def draw_broad(self):
+        for i in range(ROW):
+            for j in range(COLUMN):
+                if (i + j) % 2 == 0:
+                    pg.draw.rect(self.screen, WHITE, (i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                else:
+                    pg.draw.rect(self.screen, BLACK, (i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+        pg.display.flip()
+
+    def show_start_screen(self):
+        # game splash/start screen
+        pass
+
+    def show_go_screen(self):
+        # game over/continue
+        pass
+
+
