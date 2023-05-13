ab59ac7bf57a9d61dd87a4c5c93a8d757397738a
Nguyen Van Nam <nam.nv205106@gmail.com>
2023-05-14 00:00:18 +0700

    test

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-# import sys
+import sys
 
-# import pygame as pg
-# import ui.view1 as view1
-# import game.game as game
+import pygame as pg
+import ui.view1 as view1
+import game.game as game
 if __name__ == "__main__":
-    # view1.mainloop()
-    # g = game.Game()
+    view1.mainloop()
+    g = game.Game()
 
-    # while g.running:
-    #     g.new()
-    # pg.quit()
-    # sys.exit()
-    from ui import view1
+    while g.running:
+        g.new()
+    pg.quit()
+    sys.exit()
