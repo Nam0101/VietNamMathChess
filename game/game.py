d90fd56aca8fa0f1cbbff5f53f47c9c99951adbb
CanhThang1192002 <canhthang1192002@gmail.com>
2023-06-16 22:45:21 +0700

    thang

--- game/game.py
+++ game/game.py
@@ -9,8 +9,8 @@ WIDTH = 576
 HEIGHT = 704
 TITLE = "Cờ Toán Việt Nam"
 FPS = 15
-FONT_NAME = 'arial'
-icon = 'img/logo.jfif'
+FONT_NAME = "arial"
+icon = "img/logo.jfif"
 COLUMN = 9
 ROW = 11
 SQUARE_SIZE = HEIGHT // ROW
@@ -18,11 +18,7 @@ INF = 1000000000
 color = [(238, 238, 210), (118, 150, 86)]
 
 
-
-
-
 class Game:
-
     def __init__(self):
         self.state = state.State()
         # initialize game window,
@@ -42,13 +38,36 @@ class Game:
         self.font_name = pg.font.match_font(FONT_NAME)
         self.clock = pg.time.Clock()
         self.running = True
-        self.piece = ["b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9",
-                      "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9"]
+        self.piece = [
+            "b0",
+            "b1",
+            "b2",
+            "b3",
+            "b4",
+            "b5",
+            "b6",
+            "b7",
+            "b8",
+            "b9",
+            "r0",
+            "r1",
+            "r2",
+            "r3",
+            "r4",
+            "r5",
+            "r6",
+            "r7",
+            "r8",
+            "r9",
+        ]
         self.piece_img = {}
         for i in range(len(self.piece)):
-            self.piece_img[self.piece[i]] = pg.image.load("img/" + self.piece[i] + ".png").convert_alpha()
-            self.piece_img[self.piece[i]] = pg.transform.scale(self.piece_img[self.piece[i]],
-                                                               (SQUARE_SIZE, SQUARE_SIZE))
+            self.piece_img[self.piece[i]] = pg.image.load(
+                "img/" + self.piece[i] + ".png"
+            ).convert_alpha()
+            self.piece_img[self.piece[i]] = pg.transform.scale(
+                self.piece_img[self.piece[i]], (SQUARE_SIZE, SQUARE_SIZE)
+            )
 
     def new(self):
         # start a new game
@@ -88,7 +107,9 @@ class Game:
                     self.player_clicks.append(self.selected_square)
                 if len(self.player_clicks) == 2:
                     self.previous_square = self.player_clicks[0]
-                    movement = move.Move(self.player_clicks[0], self.player_clicks[1], self.state.board)
+                    movement = move.Move(
+                        self.player_clicks[0], self.player_clicks[1], self.state.board
+                    )
                     self.move_made = True
                     self.state.make_move(movement, self.valid_moves)
                     print(self.state.red_score, self.state.blue_score)
@@ -108,16 +129,23 @@ class Game:
     def draw_board(self):
         for i in range(ROW):
             for j in range(COLUMN):
-                pg.draw.rect(self.screen, color[(i + j) % 2],
-                             pg.Rect(j * SQUARE_SIZE, i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                pg.draw.rect(
+                    self.screen,
+                    color[(i + j) % 2],
+                    pg.Rect(j * SQUARE_SIZE, i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),
+                )
 
     def draw_piece(self):
         for i in range(ROW):
             for j in range(COLUMN):
                 piece = self.state.board[i][j]
                 if piece != "--":
-                    self.screen.blit(self.piece_img[piece],
-                                     pg.Rect(j * SQUARE_SIZE, i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                    self.screen.blit(
+                        self.piece_img[piece],
+                        pg.Rect(
+                            j * SQUARE_SIZE, i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE
+                        ),
+                    )
 
     def draw_state(self):
         self.draw_board()
@@ -131,12 +159,30 @@ class Game:
     def high_light(self):
         if self.selected_square != () and self.selected_square != (INF, INF):
             row, col = self.selected_square
-            if self.state.board[row][col][0] == ('r' if self.state.red_turn else 'b'):
-                pg.draw.rect(self.screen, self.highlight_color[0 if self.state.red_turn else 1],
-                             (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 4)
+            if self.state.board[row][col][0] == ("r" if self.state.red_turn else "b"):
+                pg.draw.rect(
+                    self.screen,
+                    self.highlight_color[0 if self.state.red_turn else 1],
+                    (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),
+                    4,
+                )
                 self.previous_square = (row, col)
                 for movement in self.valid_moves:
                     if movement.start_row == row and movement.start_col == col:
-                        pg.draw.rect(self.screen, self.highlight_color[0 if self.state.red_turn else 1],
-                                     (movement.end_col * SQUARE_SIZE, movement.end_row * SQUARE_SIZE,
-                                      SQUARE_SIZE, SQUARE_SIZE), 4)
+                        pg.draw.rect(
+                            self.screen,
+                            self.highlight_color[0 if self.state.red_turn else 1],
+                            (
+                                movement.end_col * SQUARE_SIZE,
+                                movement.end_row * SQUARE_SIZE,
+                                SQUARE_SIZE,
+                                SQUARE_SIZE,
+                            ),
+                            4,
+                        )
+
+    def getRedScore(self):
+        return self.state.red_score
+
+    def getBlueScore(self):
+        return self.state.blue_score
