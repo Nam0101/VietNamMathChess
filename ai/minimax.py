5d9da2d0673fd501151ffd511259aef1a340abbe
Nguyen Van Nam <nam.nv205106@gmail.com>
2023-06-24 16:38:57 +0700

    abc

--- ai/minimax.py
+++ ai/minimax.py
@@ -21,7 +21,7 @@ class minimax(AI):
 
     def minimax_move(self, depth, state, alpha, beta, maximizingPlayer, start_time):
         self.state_visited += 1
-        if depth == 0 or state.game_over() :
+        if depth == 0 or state.game_over():
             zobrist_hash = self.zh.calculate_zobrist_hash(state.board)
             if zobrist_hash in self.transposition_table:
                 self.state_found += 1
@@ -72,12 +72,57 @@ class minimax(AI):
                         break
             return min_score
 
+    def minimax_move_noAlphaBeta(self, depth, state, maximizingPlayer):
+        self.state_visited += 1
+        if depth == 0 or state.game_over():
+            zobrist_hash = self.zh.calculate_zobrist_hash(state.board)
+            if zobrist_hash in self.transposition_table:
+                self.state_found += 1
+                return self.transposition_table[zobrist_hash]
+            else:
+                evaluation = self.evaluation(state)
+                self.transposition_table[zobrist_hash] = evaluation
+                return evaluation
+        if maximizingPlayer:
+            max_score = -self.checkmate
+            valid_moves = state.get_all_possible_move()
+            sorted_moves = (player_move for player_move in
+                            sorted(valid_moves, key=lambda moves: self.evaluate_move(moves, state),
+                                   reverse=state.red_turn))
+            print("Valid moves: ", len(valid_moves))
+            for move in sorted_moves:
+                state.make_move(move)
+                eval_score = self.minimax_move_noAlphaBeta(depth - 1, state, False)
+                state.undo_move()
+                if eval_score > max_score:
+                    max_score = eval_score
+                    if depth == self.DEPTH:
+                        self.next_move = move
+            return max_score
+        else:
+            min_score = self.checkmate
+            valid_moves = state.get_all_possible_move()
+            sorted_moves = (player_move for player_move in
+                            sorted(valid_moves, key=lambda moves: self.evaluate_move(moves, state),
+                                   reverse=state.red_turn))
+            print("Valid moves: ", len(valid_moves))
+            for move in sorted_moves:
+                state.make_move(move)
+                eval_score = self.minimax_move_noAlphaBeta(depth - 1, state, True)
+                state.undo_move()
+                if eval_score < min_score:
+                    min_score = eval_score
+                    if depth == self.DEPTH:
+                        self.next_move = move
+            return min_score
+
     def AI_find_move(self, state, valid_moves):
         alpha = -self.checkmate
         beta = self.checkmate
         print("Finding moves with minimax, depth = ", self.DEPTH, "...")
         start_time = time.time()
-        score = self.minimax_move(self.DEPTH, state, alpha, beta, state.red_turn, start_time)
+        # score = self.minimax_move(self.DEPTH, state, alpha, beta, state.red_turn, start_time)
+        score = self.minimax_move_noAlphaBeta(2, state, state.red_turn)
         # score = self.mtdf(0, self.DEPTH, state)
         end_time = time.time()
         print("Time used: ", end_time - start_time)
